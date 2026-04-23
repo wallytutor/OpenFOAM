86abef222e987f457b228b2fb1820877a8a69a61
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-04-23 07:40:33 +0200

    Dabbling

--- run/regenerativeHeatExchanger/model-cowper-like/progress.py
+++ run/regenerativeHeatExchanger/model-cowper-like/progress.py
@@ -30,20 +30,65 @@ import pyvista as pv
 from heat_recovery.cowper_like import CowperLikePost
 from heat_recovery.thermocline import ThermoclineModel
 
-def plot_temperature_field(post):
+
+def plot_temperature_field(post, field):
     opts = post.get_options()
     pl = pv.Plotter()
 
-    # pl.add_mesh(post.slice_fluid, scalars="T", cmap="hot", **opts)
-    pl.add_mesh(post.slice_solid, scalars="T", cmap="hot", **opts)
-    CowperLikePost.align_camera(pl, xc=0.025, zc=0.02, ps=0.017)
+    match field:
+        case "fluid":
+             pl.add_mesh(post.slice_fluid, scalars="T", cmap="hot", **opts)
+        case "solid":
+             pl.add_mesh(post.slice_solid, scalars="T", cmap="hot", **opts)
+        case _:
+            pl.add_mesh(post.slice_fluid, scalars="T", cmap="hot", **opts)
+            pl.add_mesh(post.slice_solid, scalars="T", cmap="hot", **opts)
 
+    CowperLikePost.align_camera(pl, xc=0.025, zc=0.02, ps=0.017)
+    pl.show()
+
+
+def plot_field_by_key(post, name):
+    opts = post.get_options()
+    pl = pv.Plotter()
+    pl.add_mesh(post.slice_fluid, scalars=name, cmap="jet", **opts)
+    CowperLikePost.align_camera(pl, xc=0.0125, zc=0.02, ps=0.017)
     pl.show()
 
 
 model = ThermoclineModel("dimensioning.yaml")
 
-post = CowperLikePost(scale=(1, 1, model.num_D_h / model.num_h_t))
+stretch_z = model.num_D_h / model.num_h_t
+
+post = CowperLikePost(scale=(1, 1, stretch_z))
 post.load_state()
 
-plot_temperature_field(post)
+plot_temperature_field(post, "solid_fluid")
+
+plot_field_by_key(post, "p")
+plot_field_by_key(post, "p_rgh")
+plot_field_by_key(post, "rho")
+
+fluid_base = post._fluid_internal.copy()
+U_original = fluid_base["U"].copy()
+
+fluid = fluid_base.scale((1, 1, stretch_z), inplace=False)
+fluid = fluid.sample(fluid_base)
+
+# fluid.cell_data["U_original"] = U_original
+
+solid = post._solid_internal.copy()
+solid = solid.scale((1, 1, stretch_z), inplace=False)
+
+fluid.plot(scalars="U", cmap="jet", component=2)
+
+# fluid.plot(scalars="T")
+# solid.plot(scalars="T", cmap="hot")
+
+# opts = post.get_options()
+# pl = pv.Plotter()
+# pl.add_mesh(post.slice_fluid, scalars="Umag", cmap="jet", **opts)
+# pl.add_mesh(post.slice_fluid, scalars="U", component=2, cmap="jet", **opts)
+# pl.add_mesh(post.slice_fluid, scalars="U", component=2, cmap="jet", **opts)
+# CowperLikePost.align_camera(pl, xc=0.0125, zc=0.02, ps=0.017)
+# pl.show()
