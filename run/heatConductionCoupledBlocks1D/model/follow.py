b29d68d8e856ce4197c13ecc2a9677d1a1feb7fe
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-04-23 16:35:23 +0200

    WIP

--- run/heatConductionCoupledBlocks1D/model/follow.py
+++ run/heatConductionCoupledBlocks1D/model/follow.py
@@ -1,33 +1,64 @@
 # -*- coding: utf-8 -*-
-import pyvista as pv
 import numpy as np
+import pandas as pd
+import pyvista as pv
 from majordome_utilities.plotting import plot2d
+from matplotlib import pyplot as plt
 
 
-def get_profile(region):
+def _get_profile(region):
     z = np.array(region.cell_centers().points[:, 2].tolist())
     T = np.array(region.cell_data["T"].tolist())
     return z, T
 
 
+def get_profile(grid):
+    block = grid["block"]["internalMesh"]
+    media = grid["media"]["internalMesh"]
+
+    z_block, T_block = _get_profile(block)
+    z_media, T_media = _get_profile(media)
+
+    z = np.concatenate((z_block, z_media))
+    T = np.concatenate((T_block, T_media))
+    F = (T - 273.15) * 9/5 + 32
+    return z, F
+
+
+def tabulate_profile(reader):
+    matrix = []
+
+    for this_time in reader.time_values[1:]:
+        reader.set_active_time_value(this_time)
+        matrix.append(get_profile(reader.read())[1])
+
+    matrix = np.array(matrix)
+    return matrix
+
+
 reader = pv.POpenFOAMReader("case.foam")
-times = reader.time_values
 
-this_time = times[-1]
-reader.set_active_time_value(this_time)
+reader.set_active_time_value(reader.time_values[1])
 
-grid = reader.read()
+z, _ = get_profile(reader.read())
 
-block = grid["block"]["internalMesh"]
-media = grid["media"]["internalMesh"]
+data = tabulate_profile(reader)
 
-z_block, T_block = get_profile(block)
-z_media, T_media = get_profile(media)
 
-z = np.concatenate((z_block, z_media))
-T = np.concatenate((T_block, T_media))
-F = (T - 273.15) * 9/5 + 32
+selected = data[:, z < 0.05]
 
-plot = plot2d(z, F)
-plot.axes[0].set_xlim(0, 0.2)
-# block.plot(scalars="T", cmap="hot", show_edges=True)
+# plot = plot2d(z, data[-1, :])
+# plot.axes[0].set_xlim(0, 0.2)
+# plot.show()
+
+z_lims = (0.00, 0.05)
+t_lims = (reader.time_values[1] / 60, reader.time_values[-1] / 60)
+extent = (z_lims[0], z_lims[1], t_lims[0], t_lims[1])
+
+plt.close("all")
+plt.imshow(selected, aspect="auto", extent=extent, origin="lower", cmap="hot")
+plt.xlabel("z [m]")
+plt.ylabel("time [min]")
+plt.title("Temperature [°F]")
+plt.colorbar(label="Temperature [°F]")
+plt.show()
