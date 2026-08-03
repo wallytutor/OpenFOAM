093f0afaa40130a43bd1d7f1f22ba7419e410ac4
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-08-03 13:25:45 +0200

    Added sample

---
+++ run/simpleGeometriesDatabase/wedge-horizontal-cht.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Run from this directory:
+cd ${0%/*} || exit 1
+
+# To open with ParaView:
+touch case.foam
+
+# Convert base Gmsh mesh to OpenFOAM:
+gmshToFoam wedge-horizontal-cht.msh
+
+# Perform preliminary renumbering:
+renumberMesh
+
+# Check global mesh:
+checkMesh -allTopology
+
+
+# Split regions:
+splitMeshRegions -cellZones
+
+# Clean original converted mesh:
+rm -rf constant/polyMesh
+
+# Fix patch types after splitting:
+file=constant/solid/polyMesh/boundary
+foamDictionary $file -entry entry0/wedgeSolid/type -set wedge
+foamDictionary $file -entry entry0/wallSolid/type  -set wall
+
+file=constant/fluid/polyMesh/boundary
+foamDictionary $file -entry entry0/wedgeFluid/type -set wedge
+foamDictionary $file -entry entry0/surfFluid/type  -set wall
+
+# Check mesh (it will fail!):
+checkMesh -region solid
+checkMesh -region fluid
