757050ea850d1896e72a7c694c295d904d2bb417
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-08-03 11:53:24 +0200

    Added wedge

---
+++ run/simpleGeometriesDatabase/wedge-expansion-pipe.py
@@ -0,0 +1,124 @@
+# -*- encoding: utf-8 -*-
+
+import math
+import gmsh
+
+from pathlib import Path
+
+name = Path(__file__).stem
+wedge_angle = math.radians(5.0)
+
+pipe_dia = 0.025
+pipe_len = 0.15
+out_len  = 0.5 - pipe_len
+
+# TODO compute q based on b and diameter
+b        = 0.25
+q        = 0.75
+c        = 1.05
+
+rho_ax   = 20 / pipe_len
+pipe_ny  = 15
+exp_ny   = 10
+
+pipe_nx  = round(1 + int(rho_ax * pipe_len))
+out_nx   = round(1 + int(rho_ax * out_len ))
+
+# 1. Initialize the model:
+gmsh.initialize()
+gmsh.model.add(name)
+
+# 2. Get practical aliases:
+opt = gmsh.option
+mod = gmsh.model
+occ = gmsh.model.occ
+msh = gmsh.model.mesh
+
+# 3. Configure gmsh options:
+opt.set_number("Geometry.Points", 0)
+opt.set_number("Geometry.Lines", 1)
+opt.set_number("Geometry.Surfaces", 1)
+opt.set_number("Mesh.Algorithm", 6)
+opt.set_number("Mesh.SurfaceFaces", 1)
+opt.set_number("Mesh.ColorCarousel", 2)
+opt.set_number("Mesh.ElementOrder", 1)      # XXX: important!
+opt.set_number("Mesh.MshFileVersion", 2.2)  # XXX: important!
+
+# 4. Create base geometry:
+occ.add_rectangle(0,        pipe_dia, 0, pipe_len,  pipe_dia, tag=1)
+occ.add_rectangle(pipe_len, pipe_dia, 0, out_len,   pipe_dia, tag=2)
+occ.add_rectangle(pipe_len, 0,        0, out_len,   pipe_dia, tag=3)
+
+occ.rotate(
+    dimTags = [(2, 1), (2, 2), (2, 3)],
+    x       = 0,
+    y       = 0,
+    z       = 0,
+    ax      = 1,
+    ay      = 0,
+    az      = 0,
+    angle   = -wedge_angle / 2.0
+)
+occ.synchronize()
+
+# 5. Ensure shared boundaries are merged:
+occ.fragment([(2, 1), (2, 2), (2, 3)], [])
+occ.synchronize()
+
+# 6. Discretize before going 3-D:
+msh.set_transfinite_curve(1, pipe_nx, "Progression", 1.0)
+msh.set_transfinite_curve(3, pipe_nx, "Progression", 1.0)
+
+msh.set_transfinite_curve(5, out_nx,  "Progression", c)
+msh.set_transfinite_curve(7, out_nx,  "Progression", 1/c)
+msh.set_transfinite_curve(8, out_nx,  "Progression", c)
+
+msh.set_transfinite_curve(2, pipe_ny, "Bump", b)
+msh.set_transfinite_curve(4, pipe_ny, "Bump", b)
+# msh.set_transfinite_curve(6, pipe_ny, "Bump", b)
+msh.set_transfinite_curve(6, pipe_ny, "Progression", 1.0)
+
+# msh.set_transfinite_curve(9,  exp_ny, "Progression", q)
+msh.set_transfinite_curve(9,  exp_ny, "Progression", 1.0)
+msh.set_transfinite_curve(10, exp_ny, "Progression", 1/q)
+
+msh.set_transfinite_surface(tag=1)
+msh.set_transfinite_surface(tag=2)
+msh.set_transfinite_surface(tag=3)
+
+msh.set_recombine(dim=2, tag=1)
+msh.set_recombine(dim=2, tag=2)
+msh.set_recombine(dim=2, tag=3)
+
+# 7. Key step of revolution to get 3-D:
+occ.revolve(
+    dimTags     = [(2, 1), (2, 2), (2, 3)],
+    x           = 0,
+    y           = 0,
+    z           = 0,
+    ax          = 1,
+    ay          = 0,
+    az          = 0,
+    angle       = wedge_angle,
+    numElements = [1],
+    recombine   = True
+)
+occ.synchronize()
+
+# 7. Provide labels to the domain:
+fnb = [1, 2, 3, 8, 12, 15]
+vol = [1, 2, 3]
+mod.add_physical_group(dim=2, tags=[7],      tag=10, name="inlet")
+mod.add_physical_group(dim=2, tags=[10, 13], tag=20, name="outlet")
+mod.add_physical_group(dim=2, tags=[6, 11],  tag=30, name="wallExt")
+mod.add_physical_group(dim=2, tags=[4, 14],  tag=40, name="wallInt")
+mod.add_physical_group(dim=2, tags=fnb,      tag=50, name="frontAndBack")
+mod.add_physical_group(dim=3, tags=vol,      tag=100, name="volume")
+
+# # 8. Generate mesh and dump to file:
+msh.generate(dim=3)
+gmsh.write(f"{name}.msh")
+
+# 9. Display and finalize:
+gmsh.fltk.run()
+gmsh.finalize()
